1170ba7623f5125a6541ca6f9ce40f9459e577ac
Salama-Malek <salamahassanein@gmail.com>
2024-11-05 02:54:48 +0300

    added custom filters to allow edit design of add_analysis page

--- analysis/templatetags/custom_filters.py
+++ analysis/templatetags/custom_filters.py
@@ -1,8 +1,17 @@
+# analysis/templatetags/custom_filters.py
 from django import template
-
 register = template.Library()
 
 @register.filter(name='endswith')
 def endswith(value, arg):
     """Returns True if the value ends with the argument string."""
     return str(value).lower().endswith(arg)
+
+
+
+
+register = template.Library()
+
+@register.filter(name='add_class')
+def add_class(field, css):
+    return field.as_widget(attrs={"class": css})
