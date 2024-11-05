b41a1f52d753ba4d9e9f6ab7585fd3b4b5e80039
Salama-Malek <salamahassanein@gmail.com>
2024-11-05 15:25:50 +0300

    fixed design of some pages in admin dashboard

--- analysis/templatetags/custom_filters.py
+++ analysis/templatetags/custom_filters.py
@@ -1,4 +1,6 @@
 # analysis/templatetags/custom_filters.py
+import os
+
 from django import template
 register = template.Library()
 
@@ -7,6 +9,10 @@ def endswith(value, arg):
     """Returns True if the value ends with the argument string."""
     return str(value).lower().endswith(arg)
 
+@register.filter
+def basename(value):
+    """Returns the base name of the file path"""
+    return os.path.basename(value)
 
 # @register.filter(name='add_class')
 # def add_class(field, css):
